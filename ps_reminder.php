6720b73e34120300d2890332afc5681897f647b6
MathiasReker <mathias@reker.dk>
2019-02-04 12:43:05 +0100

    Combine consecutive issets

--- ps_reminder.php
+++ ps_reminder.php
@@ -697,8 +697,8 @@ class Ps_Reminder extends Module
         foreach ($stats_array as $date_stat => $array) {
             $rates = array();
             for ($i = 1; $i != 5; $i++) {
-                if (isset($stats_array[$date_stat][$i]['nb'])
-                    && isset($stats_array[$date_stat][$i]['nb_used'])
+                if (isset($stats_array[$date_stat][$i]['nb'], $stats_array[$date_stat][$i]['nb_used'])
+                     
                     && 0 < $stats_array[$date_stat][$i]['nb_used']) {
                     $rates[$i] = number_format(
                         ($stats_array[$date_stat][$i]['nb_used']
